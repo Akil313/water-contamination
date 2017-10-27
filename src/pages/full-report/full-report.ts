22cc0358822a00f47500ceb123f9ceb75e7388fb
kmn5409 <nichols.keanu9@gmail.com>
2017-10-27 02:27:55 -0400

    Update full-report.ts
    Added information for the navigation page

--- src/pages/full-report/full-report.ts
+++ src/pages/full-report/full-report.ts
@@ -10,18 +10,34 @@ import { Subject } from 'rxjs/Subject';
 import { AngularFireAuth } from 'angularfire2/auth';
 import 'rxjs/add/operator/switchMap';
 import firebase from 'firebase/app';
+var i = 0;
  
 @Component({
-  selector: 'page full-report',
-  templateUrl: 'full-report.html'
+	template:`
+		<ion-header>
+  			<ion-navbar>
+    			<ion-title>Full Report</ion-title>
+  			</ion-navbar>  
+		</ion-header>
+		
+		<ion-content>
+  			<ion-list>
+			<span *ngIf="k">
+	  					<h1>Date: {{ k.date}}</h1>
+						<h1>Location: {{ k.location}}</h1>
+						<h1>Contamination Level: {{ k.level}}%</h1>
+						
+			</span>
+  			</ion-list>  
+		</ion-content>
+	`
 })
 
 export class NavigationDetailsPage {
+	public  k: Observable<any[]>;
+  constructor(public navParams: NavParams) {
+	  this.k = navParams.get('k');
 
-  constructor(params: NavParams) {
-  }
-  ionViewDidLoad(){
-	console.log("navigation");
   }
 }
 
@@ -37,11 +53,11 @@ export class NavigationDetailsPage {
 		<ion-content>
   			<ion-list>
       			<span *ngFor="let k of temp| async">
-	  				<button ion-item block  (click)="openNavDetailsPage(item)" icon-start style="text-align:center;">
+	  				<button ion-item block  (click)="openNavDetailsPage(k)" icon-start style="text-align:center;">
 	  					<p>{{ k.date }}</p>
       					<div style="font-size: 14px">
         					<h2>Location: <strong>{{ k.location }}</strong></h2>
-        					<h2>Contamination Level: <strong>{{ k.level }}</strong></h2>
+        					<h2>Contamination Level: <strong>{{ k.level }}%</strong></h2>
         					<br>
       					</div>
       					<p>tap for more details</p>
@@ -54,20 +70,22 @@ export class NavigationDetailsPage {
 
 export class FullReportPage {
 	public  temp: Observable<any[]>;
+	public j;
 	constructor(public nav: NavController, public firebaseProvider: FirebaseProvider, public afd: AngularFireDatabase,public afAuth: AngularFireAuth) {
 		this.afAuth.authState.subscribe((user: firebase.User) => {
 			var userId = user.uid;
 	        var dataLoc = "user/"+userId+"/report";
 			this.temp = this.afd.list(dataLoc).valueChanges().map(p =>{
-				return p;
+				return p.reverse();
 			});
 	  	});
 	}
 		//console.log(afd.list('/Date', ref => ref.limitToFirst(2)));
 		//bks[1] = afd.list('/Date');
 		//console.log(bks[1]);
-
-	openNavDetailsPage() {
-		this.nav.push(NavigationDetailsPage	);
+	openNavDetailsPage( k) {
+		this.nav.push(NavigationDetailsPage,{
+		k});
+		
 	}
 }
