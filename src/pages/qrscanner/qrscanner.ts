db37bc0411d55a47d2ece79446094b5b6c8b9f20
kmn5409 <nichols.keanu9@gmail.com>
2017-10-26 23:53:41 -0400

    Update qrscanner.ts
    Added the QrScanner

--- src/pages/qrscanner/qrscanner.ts
+++ src/pages/qrscanner/qrscanner.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { AngularFireDatabase, AngularFireList,AngularFireObject } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 /**
  * Generated class for the QrscannerPage page.
@@ -14,9 +22,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'qrscanner.html',
 })
 export class QrscannerPage {
+  public itemsRef: AngularFireList<any>;
+  public items: Observable<any[]>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController, public navParams: NavParams, public androidPermissions: AndroidPermissions, private barcodeScanner: BarcodeScanner,
+	public db: AngularFireDatabase,public afAuth: AngularFireAuth) { }
+
+    scannedCode = null;
+
+
+    scanCode(){
+		this.afAuth.authState.subscribe( user => {
+		  this.itemsRef = this.db.list('device/1');
+		  this.barcodeScanner.scan().then((barcodeData) => {
+			  var userId = user.uid;
+			this.scannedCode = barcodeData.text;
+			var code = barcodeData.text;
+			{
+					this.itemsRef.push({ ID:code,Owner:userId  });
+			}
+		  }, (err) => {
+			alert("Text");
+		  });
+		});
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad QrscannerPage');
